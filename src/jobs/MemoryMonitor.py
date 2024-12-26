291c54a4fcb9a87b5bd5343ec75005f54057818b
IcQuackson <pedrorenato1998@gmail.com>
2024-12-26 17:28:18 +0000

    revamp memory monitor
    - deleted memory monitor thread.
    - Changed memory limit using setrlimit and catch MemoryError exception

--- src/jobs/MemoryMonitor.py
+++ src/jobs/MemoryMonitor.py
@@ -2,25 +2,17 @@ import time
 import logging
 import os
 import psutil
+import resource
 
 logger = logging.getLogger()
-
-def handle_memory_monitor():
-	monitor = MemoryMonitor()
-
-	from main import shutdown_event
-	while not shutdown_event.is_set():
-		logger.info(f"Memory usage: {monitor.get_memory_usage()} MB")
-
-		if monitor.has_memory_exceeded_limit():
-			logger.info(f"Memory has exceed the {monitor.get_memory_limit()} MB limit. Shuting down...")
-			os._exit(1)
-			
-		time.sleep(2)
 
 class MemoryMonitor:
 	def __init__(self, memory_limit_mb=100):
 		self.memory_limit_mb = memory_limit_mb
+
+	def set_memory_limit(self):
+		max_mem_bytes = self.memory_limit_mb * 1024 * 1024
+		resource.setrlimit(resource.RLIMIT_AS, (max_mem_bytes, max_mem_bytes))
 
 	def get_memory_limit(self):
 		return self.memory_limit_mb
--- src/main.py
+++ src/main.py
@@ -9,7 +9,7 @@ import utils
 from logger_setup import setup_logger
 import logging
 from logging.handlers import RotatingFileHandler
-from jobs.MemoryMonitor import handle_memory_monitor
+from jobs.MemoryMonitor import handle_memory_monitor, MemoryMonitor
 from utils import signal_handler
 
 
@@ -17,20 +17,29 @@ os.makedirs("/var/log/irondome", exist_ok=True)
 shutdown_event = threading.Event()
 
 def main():
-	setup_logger()
-	logger = logging.getLogger()
 
-	t1 = threading.Thread(target=handle_memory_monitor, daemon=True)
-	#t2 = threading.Thread(target=feature2, daemon=True)
-	t1.start()
-	#t2.start()
+	monitor = MemoryMonitor()
+	monitor.set_memory_limit()
 
-	""" 	l = []
-	while not shutdown_event.is_set(): # This tests memory usage
-		l.append("*" * 1024) """
+	try:
 
-	while not shutdown_event.is_set():
-		time.sleep(1)
+		setup_logger()
+		logger = logging.getLogger()
+
+		#t1 = threading.Thread(target=handle_memory_monitor, daemon=True)
+		#t2 = threading.Thread(target=feature2, daemon=True)
+		#t1.start()
+		#t2.start()
+
+		l = []
+		while not shutdown_event.is_set(): # This tests memory usage
+			l.append("*" * 1024)
+
+		while not shutdown_event.is_set():
+			time.sleep(1)
+	except MemoryError:
+		logger.info(f"Memory has exceed the {monitor.get_memory_limit()} MB limit. Shuting down...")
+
 
 
 if __name__ == "__main__":
